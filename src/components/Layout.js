a32bcac286192a7d070be6a45fb804404c0a9c88
whitefog425 <127935071+whitefog425dev@users.noreply.github.com>
2025-09-29 02:34:43 -0400

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Layout = ({children}) => {
   return (
     <div className='border-4 border-red-500 w-[200px] rounded'>
+    
       {children}
+    
     </div>
   )
 }
